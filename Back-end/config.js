6b5aa7bdeb2494cc30a8160b13c0fb83d280ef54
CamiloLopez15 <camilolopez1506@gmail.com>
2023-11-27 17:39:13 -0500

    Change gitignore

---
+++ Back-end/config.js
@@ -0,0 +1,3 @@
+export const passEmail = process.env.PASS_EMAIL ||"qzuelortlcojvyex";
+export const email = process.env.EMAIL || "camilolopez1506@gmail.com";
+export const port = process.env.PORT || 8000;
